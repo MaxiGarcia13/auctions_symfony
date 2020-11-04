bf773b93026e652b0c053556cf5bf5e996ab1f6d
Maximiliano Garcia <maxig8@hotmail.com>
2020-11-04 11:06:42 +0100

    Se terminaron los endpoint de auction

--- src/Controller/AuctionController.php
+++ src/Controller/AuctionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
+use App\Entity\Auction;
+use App\Repository\AuctionRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +18,7 @@ class AuctionController extends AbstractController
      *
      * @return void
      */
-    public function getCreateAuctions(Request $request)
+    public function createAuction(Request $request, EntityManagerInterface $em)
     {
 
         $response = new JsonResponse();
@@ -25,9 +28,40 @@ class AuctionController extends AbstractController
             return $response;
         }
 
+        $data = json_decode($request->getContent(), true);
+
+        $name = $data['name'];
+        $desciption = $data['desciption'];
+        $picture = $data['picture'];
+        $initialValue = $data['initial_value'];
+
         if ('POST' === $request->getMethod()) {
 
             try {
+                $auction = new Auction();
+
+                $auction->setName($name);
+                $auction->setDescription($desciption);
+                $auction->setPicture($picture);
+                $auction->setInitialValue($initialValue);
+                $auction->setCreated(new DateTime());
+
+                $em->persist($auction);
+                $em->flush();
+
+                $response->setData([
+                    'success' => true,
+                    'data' => [
+                        [
+                            'id' => $auction->getId(),
+                            'name' => $auction->getName(),
+                            'description' => $auction->getDescription(),
+                            'picture' => $auction->getPicture(),
+                            'initial_value' => $auction->getInitialValue(),
+                            'created' => $auction->getCreated()
+                        ]
+                    ]
+                ]);
             } catch (\Throwable $th) {
                 $response->setData([
                     'success' => false
@@ -36,6 +70,71 @@ class AuctionController extends AbstractController
             }
         } else {
             $response->setStatusCode(400);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @Route("/auction/{id}", name="auction_managment")
+     *
+     * @return void
+     */
+    public function auctionManagment($id, Request $request, AuctionRepository $auctionRepository, EntityManagerInterface $em)
+    {
+
+        $response = new JsonResponse();
+
+        if (!$this->getUser()) {
+            $response->setStatusCode(403);
+            return $response;
+        }
+
+        $auction = $auctionRepository->find($id);
+
+        switch ($request->getMethod()) {
+            case 'PUT':
+                try {
+
+                    $data = json_decode($request->getContent(), true);
+
+                    $auction->setName($data['name']);
+                    $auction->setDescription($data['desciption']);
+                    $auction->setPicture($data['picture']);
+                    $auction->setInitialValue($data['initial_value']);
+
+                    $em->persist($auction);
+                    $em->flush();
+
+                    $response->setData([
+                        'success' => true,
+                        'data' => [
+                            [
+                                'id' => $auction->getId(),
+                                'name' => $auction->getName(),
+                                'description' => $auction->getDescription(),
+                                'picture' => $auction->getPicture(),
+                                'initial_value' => $auction->getInitialValue(),
+                                'created' => $auction->getCreated()
+                            ]
+                        ]
+                    ]);
+                } catch (\Throwable $th) {
+                    $response->setData([
+                        'success' => false
+                    ]);
+                    $response->setStatusCode(500);
+                }
+                break;
+
+            case 'DELETE':
+                $em->remove($auction);
+                $em->flush();
+                break;
+
+            default:
+                $response->setStatusCode(400);
+                break;
         }
 
         return $response;
